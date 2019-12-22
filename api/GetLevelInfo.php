a57576cea84ceef995b00649fee961e290ba4c9c
逸梦 <55975094+CB-ym@users.noreply.github.com>
2019-12-22 20:04:11 +0800

    Delete GetLevelInfo.php

--- api/GetLevelInfo.php
+++
@@ -1,7 +0,0 @@
-<?php
-include 'yingshi.php';
-include 'conf.php';
-$fuin=$_GET['fuin'];
-$QQ=$_GET['QQ'];
-echo $yingshi->curl_file_get_contents("http://".$ip."/v1/GetLevelInfo?qq=".$QQ."&fuin=".$fuin);
-?>
